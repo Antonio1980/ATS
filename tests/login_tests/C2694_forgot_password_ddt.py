178e58ce103ab47c3b3fa6ab6983910ba24f451d
Anton S <antons@coins.exchange>
2018-05-14 19:36:35 +0300

    forgot data driven added

--- tests/login_tests/C2694_forgot_password_ddt.py
+++ tests/login_tests/C2694_forgot_password_ddt.py
@@ -2,23 +2,27 @@
 # -*- coding: utf8 -*-
 
 import unittest
+from ddt import ddt, data, unpack
 from proboscis import test
 from tests.pages.browser import Browser
 from tests.pages.login_page import LogInPage
+from tests_sources.test_definitions import BaseConfig
+from tests_sources.test_utils.file_util import get_csv_data
 
 
 @test(groups=['end2end', 'functional', 'sanity'])
-class ForgotPasswordTest(unittest.TestCase):
+@ddt
+class ForgotPasswordTestDDT(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         Browser.setUpClass("chrome")
 
         
-    @classmethod
     @test(groups=['login_page', 'positive'])
-    def test_forgot_password(self):
-        email = "roman@spotoption.com"
-        delay = 2
+    @data(*get_csv_data(BaseConfig.W_TEST_FORGOT_DATA))
+    @unpack
+    def test_forgot_password_ddt(self, email):
+        delay = 1
         LogInPage.forgot_password(delay, email)
 
 
