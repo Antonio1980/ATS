bd26a77afeafa434f536a34ba45220fe592023fd
antons <antons@coins.exchange>
2018-06-18 09:57:04 +0300

    finilized before to switch to main project

--- tests/tests_crm_bo/tests_login/C2590_login_test.py
+++ tests/tests_crm_bo/tests_login/C2590_login_test.py
@@ -19,17 +19,18 @@ class LogInTest(unittest.TestCase):
         cls.test_case = '2590'
         cls.test_run = BaseConfig.TESTRAIL_RUN
 
+    @classmethod
     @test(groups=['login_page', 'positive'])
-    def test_login_positive(self):
+    def test_login_positive(cls):
         delay = 1
         result = False
         try:
-            result = self.login_page.login_positive(self.driver, delay)
+            result = cls.login_page.login_positive(cls.driver, delay)
         finally:
             if result is True:
-                update_test_case(self.test_run, self.test_case, 1)
+                update_test_case(cls.test_run, cls.test_case, 1)
             else:
-                update_test_case(self.test_run, self.test_case, 0)
+                update_test_case(cls.test_run, cls.test_case, 0)
 
     @classmethod
     def tearDownClass(cls):
--- tests/tests_web_platform/forgot_password_tests/C3558_forgot_password_ui_test.py
+++ tests/tests_web_platform/forgot_password_tests/C3558_forgot_password_ui_test.py
@@ -6,18 +6,19 @@ from proboscis import test
 from src.base.enums import Browsers
 from tests.drivers.webdriver_factory import WebDriverFactory
 from tests.test_definitions import BaseConfig
+from tests.tests_web_platform.locators.forgot_password_page_locators import ForgotPasswordPageLocators
+from tests.tests_web_platform.pages import forgot_password_page_url
 from tests.tests_web_platform.pages.home_page import HomePage
 from src.test_utils.testrail_utils import update_test_case
-from src.test_utils.mailinator_utils import get_mailinator_updates
-from tests.tests_web_platform.pages.open_account_page import OpenAccountPage
+from tests.tests_web_platform.pages.login_page import LogInPage
 
 
 @test(groups=['end2end_tests', 'functional', 'sanity'])
 class ForgotPasswordUiTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.login_page = LogInPage()
         cls.home_page = HomePage()
-        cls.email = OpenAccountPage().email
         cls.driver = WebDriverFactory.get_browser(Browsers.CHROME.value)
         cls.test_case = '3558'
         cls.test_run = BaseConfig.TESTRAIL_RUN
@@ -26,14 +27,18 @@ class ForgotPasswordUiTest(unittest.TestCase):
     @test(groups=['login_page', 'positive'])
     def test_forgot_password_page_ui(cls):
         delay = 1
-        result1 = False
+        result1, result2 = False, False
         try:
-            cls.home_page.open_login_page(cls.driver, delay)
-            data = get_mailinator_updates(cls.driver, cls.email)
-            print(data)
+            result1 = cls.home_page.open_login_page(cls.driver, delay)
+            result2 = cls.login_page.click_on_forgot_password(cls.driver, delay)
+            cls.login_page.driver_wait(cls.driver, delay)
+            assert forgot_password_page_url == cls.login_page.get_cur_url(cls.driver)
+            assert cls.login_page.wait_element_presented(cls.driver, delay, ForgotPasswordPageLocators.FORGOT_PASSWORD_TITLE)
+            cls.login_page.wait_element_presented(cls.driver, delay, ForgotPasswordPageLocators.EMAIL_TEXT_FIELD)
         finally:
-            if result1 is True:
-                update_test_case(cls.test_run, cls.test_case, 1)
+            if result1 & result2 is True:
+                if cls.login_page.wait_element_presented(cls.driver, delay, ForgotPasswordPageLocators.SUBMIT_BUTTON):
+                    update_test_case(cls.test_run, cls.test_case, 1)
             else:
                 update_test_case(cls.test_run, cls.test_case, 0)
 
