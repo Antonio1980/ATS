8d7ffe461c808a64ce0e734cd8878a9321a4b26d
antons <antons@coins.exchange>
2018-06-25 14:06:35 +0300

    deleted class DataBase , moved to db_utils as static method

--- src/test_utils/mailinator_utils.py
+++ src/test_utils/mailinator_utils.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import re
 import string
 import random
 
@@ -10,32 +11,48 @@ def email_generator(size=8, chars=string.ascii_lowercase + string.digits):
 
 
 def get_email_updates(driver, email, action):
-    change_password = "//a[contains(text(),'Change Password')]"
-    verify_email = "//a[contains(text(),'Verify Email')]"
-    mailinator_box_url = "http://www.mailinator.com/v2/inbox.jsp?zone=public&query=" + email
-    print(mailinator_box_url)
+    pattern2 = r"([\w\.-]+)"
+    e, = email
+    e = re.findall(pattern2, e)
+    e = e[0]
+    change_password = "//a[@href='{{.changePasswordUrl}}']"
+    verify_email = "//a[@href='{{.verifyEmailUrl}}']"
+    mailinator_box_url = "http://www.mailinator.com/v2/inbox.jsp?zone=public&query={0}".format(e)
     driver.get(mailinator_box_url)
     pause_button = driver.find_element_by_xpath("//*[@id='play_button']")
     pause_button.click()
     email = driver.find_element_by_xpath("//*[@id='inboxpane']/li[1]")
     email.click()
     driver.implicitly_wait(3)
-    # 1 - get_updates, 2 - click on change_pasword, 3 - click on verify_email
+    # 1 - get_updates, 2 - click on change_password, 3 - click on verify_email
     if action == 1:
-        _get_updates(driver)
+        return _get_updates(driver)
     elif action == 2:
-        _verify_email(driver, change_password)
+        return _click_on(driver, change_password)
     elif action == 3:
-        _verify_email(driver, verify_email)
+        return _click_on(driver, verify_email)
 
 
 def _get_updates(driver):
-    mail_content = driver.find_element_by_xpath("//*[@id='msgpane']")
-    driver.implicitly_wait(3)
-    return mail_content
+    try:
+        driver.implicitly_wait(3)
+    finally:
+        mail_content = driver.find_element_by_xpath("//*[@id='msgpane']")
+        driver.implicitly_wait(3)
+        if mail_content:
+            return mail_content
+        else:
+            return False
 
 
-def _verify_email(driver, locator):
-    button = driver.find_element_by_xpath(locator)
-    button.click()
-    driver.implicitly_wait(3)
+def _click_on(driver, locator):
+    try:
+        driver.implicitly_wait(3)
+        button = driver.find_element_by_xpath(locator)
+        button.click()
+        driver.implicitly_wait(3)
+    finally:
+        if driver:
+            return True
+        else:
+            return False
